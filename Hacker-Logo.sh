182f985a3ee92ebb12565c34319ebe944c51342a
BabyDragoN-MarMu <83784408+BabyDragoN-MarMu@users.noreply.github.com>
2021-10-29 01:48:20 +0630

    Create Hacker-Logo.sh

---
+++ Hacker-Logo.sh
@@ -0,0 +1,132 @@
+#Creation By MarMu
+#Facebook Page - Technology By MarMu
+#DO NOT THIEF MY STYLE
+#DON'T COPY & MODIFY THIS CODE
+####£££££££££££££££££££££££££################
+
+-command_not_found_handle() {
+	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+
+clear
+RD='\e[1;31m'
+
+GR='\e[1;32m'
+
+YL='\e[1;33m'
+
+BL='\e[1;34m'
+
+PP='\e[1;35m'
+
+LBL='\e[1;36m'
+
+GY='\e[1;30m'
+
+WH='\e[1;37m'
+
+clear
+figlet setup | lolcat
+#pkg installation
+pkg install ruby -y
+pkg install python -y
+pkg install figlet -y
+gem install lolcat
+
+cd $HOME
+cd 7-H4CK3R
+ls
+
+echo
+clear
+echo " "
+echo " "
+echo -e "\e[1m\e[33m\
+     ████████╗███████╗██████╗░███╗░░░███╗██╗░░░██╗██╗░░██╗
+     ╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║░░░██║╚██╗██╔╝
+     ░░░██║░░░█████╗░░██████╔╝██╔████╔██║██║░░░██║░╚███╔╝░
+     ░░░██║░░░██╔══╝░░██╔══██╗██║╚██╔╝██║██║░░░██║░██╔██╗░
+     ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║╚██████╔╝██╔╝╚██╗
+     ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝
+\e[36m\
+        ╔╗╔╗───────╔╗─────────╔══╗╔═╗╔══╗╔═╦═╗
+        ║╚╝║╔═╗─╔═╗║╠╗╔═╗╔╦╗──╚╗╔╝║╦╝║╔╗║║║║║║
+        ║╔╗║║║╚╗║═╣║═╣║╩╣║╔╝───║║─║╩╗║╠╣║║║║║║
+        ╚╝╚╝╚══╝╚═╝╚╩╝╚═╝╚╝────╚╝─╚═╝╚╝╚╝╚╩═╩╝
+
+"
+echo ""
+sleep 1.0
+pip install lolcat
+gem install lolcat
+echo " "
+echo -e $YL"      ------☆☆☆------($RD  M A R M U  $GR)------☆☆☆------"
+
+echo " "
+echo -e $PP"           《$GR H$gYL A$RD C$GY K$LBL E$GR R$WH  ☆$RD  M$BL Y$GR A$PP N$LBL M$YL A$BL R$GY 》" 
+echo
+echo -e ""
+echo -e $GR " Enter Your Name : "
+read name
+echo -e "figlet "$name" | lolcat" >name.txt
+          cat name.txt >> bash.bashrc
+echo " "
+termux-setup-storage
+pip2 install lolcat
+
+cd  $PREFIX/etc
+
+rm -rf bash.bashrc
+
+ls
+
+cd $HOME
+
+cd 7-H4CK3R
+
+cp bash.bashrc $PREFIX/etc
+
+ls
+
+chmod +x bash.bashrc
+
+ls
+       
+cd $HOME
+
+cd 7-H4CK3R
+
+pkg install figlet -y
+echo ""
+clear
+echo "                   "DO YOU KNOW MARMU ??  | lolcat
+
+pkg install toilet -y
+cd $HOME
+sleep 2
+echo
+apt update && apt upgrade -y
+pkg install nano -y
+echo " "
+sleep 0.2
+clear
+sleep 0.2
+figlet -f big  '  WE ' | lolcat
+figlet -f big '  ARE   ' | lolcat
+figlet -f big ' HACKER ' | lolcat
+
+echo "  LIKE & FOLLOW MY PAGE PLEASE" | lolcat
+echo
+sleep 0.2
+
+echo " "
+
+xdg-open https://www.facebook.com/marmu.007/
+
+cd $HOME
+
+logout
+sleep 0.2
+
+exit
+
