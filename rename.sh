042ec6a405ecd335c6925f678cf4b398ff7d9bad
BabyDragoN-MarMu <83784408+BabyDragoN-MarMu@users.noreply.github.com>
2021-11-15 13:45:23 +0630

    Create rename.sh

---
+++ rename.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#Creation - MARMU
+#Facebook - TECHNOLOGY BY MARMU
+#DO NOT COPY & MODIFY
+#####စောက်ဖောင်းထုခြင်တဲ့ခေါင်းကြီးတို့ 🦋လံုပါစေ ###############
+##### I'm Recept All Senior ##########################
+
+
+
+clear
+
+echo 
+
+echo -e "\e[1m Love You All..!!!"
+apt update
+apt upgrade -y
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+gem install lolcat
+echo $W "Installing requirements........"$G
+apt-get install curl -y
+apt-get install tor -y
+apt-get install perl -y
+apt-get install git -y
+
+echo
+        toilet --gay Ready...!! | lolcat
+        echo
+
+echo -e "\e[1m\e[35m Do You Know MARMU.."
+sleep 2.2
+cd $HOME
+rm -rf 7-H4CK3R
+
+echo
+        echo -e "\e[1m\e[32mV-2.0 \[\e[35mCreation by :\e[33m MarMu (Myanmar)"
+sleep 1.2
+echo
+cd $HOME
+git clone https://github.com/BabyDragon-MarMu/7-H4CK3R
+cd 7-H4CK3R
+chmod 777 *
+bash Hacker-logo.sh
